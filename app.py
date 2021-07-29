7e226fb46c016a99bf78c5d6e57420581e2439ec
Ariel <hirschhornariel@gmail.com>
2021-07-29 15:22:34 -0400

    progress made on 2 types of data viz, website in progress

--- app.py
+++ app.py
@@ -6,34 +6,47 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+
 import plotly.express as px
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+fig = {}
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+full_bce_df = pd.read_pickle('./full_bce_df.pkl')
+known_bce_df = pd.read_pickle('./known_bce_df.pkl')
+unknown_bce_df = pd.read_pickle('./unknown_bce_df.pkl')
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+def line_plot(df, plot_title):
+    fig = px.line(df, x='Year', y="Object Count",title=plot_title)
+    fig.update_yaxes(type='linear')
+    return fig
+                    
+full_bce = line_plot(full_bce_df, 'Full collection')
+known_bce = line_plot(known_bce_df, 'Known makers')
+unknown_bce = line_plot(unknown_bce_df, 'Unknown makers')
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    
+    html.H1(children='Website in progress'),
+    html.H3(children='Amount of objects by year, BCE'),
+    
+    dbc.Row(
+        [dbc.Col(dcc.Graph(id='bce-full',figure=full_bce)),
+        dbc.Col(dcc.Graph(id='bce-known',figure=known_bce)),
+         dbc.Col(dcc.Graph(id='bce-unknown',figure=unknown_bce))
+        ],
+    
+    ),
+            
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    
 ])
 
 if __name__ == '__main__':
