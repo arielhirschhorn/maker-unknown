3de98a181e63dea26ebe2e7b8ebc7de2021c47c6
Ariel <hirschhornariel@gmail.com>
2021-07-29 17:51:41 -0400

    website app up and running, need to organize files at some point

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 fig = {}
 
@@ -22,6 +20,10 @@ fig = {}
 full_bce_df = pd.read_pickle('./full_bce_df.pkl')
 known_bce_df = pd.read_pickle('./known_bce_df.pkl')
 unknown_bce_df = pd.read_pickle('./unknown_bce_df.pkl')
+
+full_ce_df = pd.read_pickle('./full_ce_df.pkl')
+known_ce_df = pd.read_pickle('./known_ce_df.pkl')
+unknown_ce_df = pd.read_pickle('./unknown_ce_df.pkl')
 
 def line_plot(df, plot_title):
     fig = px.line(df, x='Year', y="Object Count",title=plot_title)
@@ -32,18 +34,51 @@ full_bce = line_plot(full_bce_df, 'Full collection')
 known_bce = line_plot(known_bce_df, 'Known makers')
 unknown_bce = line_plot(unknown_bce_df, 'Unknown makers')
 
+full_ce = line_plot(full_ce_df, 'Full collection')
+known_ce = line_plot(known_ce_df, 'Known makers')
+unknown_ce = line_plot(unknown_ce_df, 'Unknown makers')
+
 app.layout = html.Div(children=[
     
     html.H1(children='Website in progress'),
-    html.H3(children='Amount of objects by year, BCE'),
     
+    
+    dbc.Row(html.H3(children='Amount of objects by year, BCE'), justify="center", align="center"),
     dbc.Row(
-        [dbc.Col(dcc.Graph(id='bce-full',figure=full_bce)),
-        dbc.Col(dcc.Graph(id='bce-known',figure=known_bce)),
-         dbc.Col(dcc.Graph(id='bce-unknown',figure=unknown_bce))
+        [dbc.Col(dcc.Graph(id='bce-full',figure=full_bce), width = 4),
+        dbc.Col(dcc.Graph(id='bce-known',figure=known_bce), width = 4),
+         dbc.Col(dcc.Graph(id='bce-unknown',figure=unknown_bce), width = 4),
         ],
     
     ),
+    
+    
+    dbc.Row(html.H3(children='Amount of objects by year, CE'), justify="center", align="center"),
+    dbc.Row(
+        [dbc.Col(dcc.Graph(id='ce-full',figure=full_ce), width = 4),
+        dbc.Col(dcc.Graph(id='ce-known',figure=known_ce), width = 4),
+         dbc.Col(dcc.Graph(id='ce-unknown',figure=unknown_ce), width = 4),
+        ],
+        justify="center",
+        
+        ),
+    dbc.Row(html.H3(children='Word Clouds: Description field'), justify="center", align="center"),
+    dbc.Row(
+        [dbc.Col([html.H4(children='Full collection'), html.Img(src=app.get_asset_url('full_collection_description.png'),style={'height':'25%'})], width = 4),
+         dbc.Col([html.H4(children='Known makers'), html.Img(src=app.get_asset_url('known_maker_description.png'),style={'height':'25%'})], width = 4),
+         dbc.Col([html.H4(children='Unknown makers'), html.Img(src=app.get_asset_url('unknown_maker_description.png'),style={'height':'25%'})], width = 4),
+        ],
+    ),
+    
+    dbc.Row(html.H3(children='Word Clouds: Title field'), justify="center", align="center"),
+    dbc.Row(
+        [dbc.Col([html.H4(children='Full collection'), html.Img(src=app.get_asset_url('full_collection_title.png'),style={'height':'25%'})], width = 4),
+         dbc.Col([html.H4(children='Known makers'), html.Img(src=app.get_asset_url('known_maker_title.png'),style={'height':'25%'})], width = 4),
+         dbc.Col([html.H4(children='Unknown makers'), html.Img(src=app.get_asset_url('unknown_maker_title.png'),style={'height':'25%'})], width = 4),
+        ],
+    ),
+        
+    
             
 
     
